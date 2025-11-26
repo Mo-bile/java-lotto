d5af92df914f87715c9cc19122ba31364104c5d8
모재영 <mo@utransfer.com>
2025-11-26 22:57:31 +0900

    fix : 출력문 수정

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -18,8 +18,9 @@ public class ResultView {
         System.out.println("당첨 통계");
         System.out.println("---------");
         System.out.printf("3개 일치 (5000원)- %d개%n", winningResult.getMatchCount(Match.FIFTH));
-        System.out.printf("4개 일치 (50000원)- %d개%n", winningResult.getMatchCount(Match.THIRD));
-        System.out.printf("5개 일치 (1500000원)- %d개%n", winningResult.getMatchCount(Match.SECOND));
+        System.out.printf("4개 일치 (50000원)- %d개%n", winningResult.getMatchCount(Match.FOURTH));
+        System.out.printf("5개 일치 (1500000원)- %d개%n", winningResult.getMatchCount(Match.THIRD));
+        System.out.printf("5개 일치, 보너스 볼 일치(30000000원) -  %d개%n", winningResult.getMatchCount(Match.SECOND));
         System.out.printf("6개 일치 (2000000000원)- %d개%n", winningResult.getMatchCount(Match.FIRST));
         if (Double.parseDouble(totalReturnRate) < 1.0) {
             System.out.printf("총 수익률은 %s입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)%n", totalReturnRate);
